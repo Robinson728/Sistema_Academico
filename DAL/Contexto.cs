89b15e46acdd5b0dbb925803c1ec2e5ca3a27681
Jonathan001-cmd <77310361+Jonathan001-cmd@users.noreply.github.com>
2021-06-05 14:05:12 -0400

    Contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Sistema_Academico.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,16 @@ namespace Sistema_Academico.DAL
 {
     class Contexto : DbContext
     {
+        public DbSet<Asignaturas> Asigatura { get; set; }
+
+        public DbSet<Calificaciones> Calificacion { get; set; }
+        public DbSet<Carreras> Carrera { get; set; }
+        public DbSet<Estudiantes> Estudiante { get; set; }
+        public DbSet<Grupos> Grupo { get; set; }
+        public DbSet<Pensum> Pensums { get; set; }
+        public DbSet<Profesores> Profesor { get; set; }
+        public DbSet<Semestres> Semestre { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite(@"Data Source = Data\GestionAcademico.db");
