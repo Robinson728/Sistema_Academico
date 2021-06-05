8ab8e8622a7cd683073dba131578ec35a4cf6ace
Jonathan001-cmd <77310361+Jonathan001-cmd@users.noreply.github.com>
2021-06-05 13:57:07 -0400

    Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Academico.DAL
+{
+    class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Data\GestionAcademico.db");
+        }
+    }
+}
