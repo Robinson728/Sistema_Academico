79706993d5b8ba5a29635fd9b5740b630e7ca3a1
Robinson728 <77537741+Robinson728@users.noreply.github.com>
2021-06-05 13:55:48 -0400

    Creando Clases.

---
+++ Models/Grupos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sistema_Academico.Models
+{
+    public class Grupos
+    {
+        [Key]
+        public int GrupoId { get; set; }
+        public string Asignatura { get; set; }
+        public string Profesor { get; set; }
+        public int CantidadEstudiantes { get; set; }
+        public string Aula { get; set; }
+        public int NumeroGrupo { get; set; }
+    }
+}
---
+++ Models/Semestres.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sistema_Academico.Models
+{
+    public class Semestres
+    {
+        [Key]
+        public int SemestreId { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+    }
+}
